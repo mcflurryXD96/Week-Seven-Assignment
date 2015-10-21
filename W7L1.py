8796fba670c7818b2643a0cead3940f763984f97
mcflurryxd96 <daniel.mcmurray@myemail.indwes.edu>
2015-10-21 18:31:18 +0000

    Completed_Week_Seven_Assignment

--- W7L1.py
+++ W7L1.py
@@ -1,5 +1,5 @@
 # Framework for Conway's game of life
-# Daniel Neumann
+# Daniel McMurray
 # CIS 125 FA2104
 
 # Functions
@@ -19,6 +19,14 @@
 
 
 def populate(petri_dish, h=80, w=22):
+    import random
+    for x in range(h):
+            row = []
+            for y in range(w):
+                    row.append(0)
+            petri_dish.append(row)
+            
+def rpopulate(petri_dish, h=80, w=22):
     import random
     for x in range(h):
             row = []
@@ -76,27 +84,26 @@ def generation(petri_dish, h=22, w=80):
             if petri_dish[x][y] == 0:
                 if n == 3:
                     new_world[x][y] = 1
-            #else:
-                #new_world[x][y] = 0
+                else:
+                    new_world[x][y] = 0
             else: #(cell is alive)
                 if n < 2 or n > 3:
                     new_world[x][y] = 0
-            # else:
-                #new_world[x][y] = 1
-    
-    print(new_world)
-    petri_dish = new_world
+                else:
+                    new_world[x][y] = 1
+
+    return new_world
 
 
 def main():
     world = []
     height = 22
     width = 80
-    populate(world, height, width)
+    rpopulate(world, height, width)
     display(world, height, width)
     key = input("Press q to quit, any other key to continue: ")
     while key != 'q':
-        generation(world, height, width)
+        world = generation(world, height, width)
         display(world, height, width)
         key = input("Press q to quit, any other key to continue: ")
 
